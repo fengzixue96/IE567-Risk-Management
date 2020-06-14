edb0ab3b57bc1a7df8fb41955fde019d959f38a2
fengzixue96 <fengzixue96@163.com>
2020-06-14 02:05:12 -0500

    Create HW1.R

---
+++ HW1/HW1.R
@@ -0,0 +1,13 @@
+# Solution to Fin 567 Homework 1 spring 2020 Question 5 part h
+#
+
+#Question 5 part h
+Q5h1 = dbinom(0, size=252, prob = 0.05)
+Q5h1p = pbinom(0, size=252, prob = 0.05)
+
+Q5h21 = dbinom(1, size=252, prob = 0.05)
+Q5h22 = dbinom(2, size=252, prob = 0.05)
+Q5h23 = pbinom(2, size =252, prob = 0.05)
+
+
+
